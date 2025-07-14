92d5eb03c151a3d1793d6ef133593a9a8c8712fa
Angad Kumar <angadkumar16ak@gmail.com>
2025-07-14 10:56:01 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { Box, Flex, Heading, HStack, Spacer, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Button,
+} from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <Box bg="white" _dark={{ bg: "gray.900" }} px={6} py={4} shadow="sm" position="sticky" top={0} zIndex={10}>
+    <Box
+      bg="white"
+      _dark={{ bg: "gray.900" }}
+      px={6}
+      py={4}
+      shadow="sm"
+      position="sticky"
+      top={0}
+      zIndex={10}
+    >
       <Flex align="center" maxW="6xl" mx="auto">
         <Heading size="md">
           <Link href="/">StudySync</Link>
