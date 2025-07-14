b2e390ac353926da4425d4534e829352b0d23b2d
Angad Kumar <angadkumar16ak@gmail.com>
2025-07-14 11:34:42 -0500

    Read me

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, localStorageManager } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [colorModeManager, setColorModeManager] = useState(undefined);
+
+  useEffect(() => {
+    //Local Storage Wait
+    setColorModeManager(localStorageManager);
+  }, []);
+
+  // Don't render anything until we know how to manage color mode
+  if (!colorModeManager) return null;
+
+  return (
+    <ChakraProvider colorModeManager={colorModeManager}>
+      {children}
+    </ChakraProvider>
+  );
 }
