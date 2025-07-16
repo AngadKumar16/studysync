c76a9dae25e170f8f0fe5d0c548e73fae6e61955
Angad Kumar <angadkumar16ak@gmail.com>
2025-07-16 15:56:57 -0500

    I think i fixed the providers issue

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,14 +4,12 @@ import { ChakraProvider, localStorageManager } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [colorModeManager, setColorModeManager] = useState(undefined);
+  const [colorModeManager, setColorModeManager] = useState<typeof localStorageManager | undefined>(undefined);
 
   useEffect(() => {
-    //Local Storage Wait
     setColorModeManager(localStorageManager);
   }, []);
 
-  // Don't render anything until we know how to manage color mode
   if (!colorModeManager) return null;
 
   return (
